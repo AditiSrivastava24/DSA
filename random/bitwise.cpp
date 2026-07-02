f18f3ae48de94bdfe2b3ec5ec4e256885f0bb2ab
AditiSrivastava24 <aditisriv24@gmail.com>
2026-07-02 23:34:51 +0530

    8

---
+++ random/bitwise.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a=6;
+    int b=2;
+
+    cout<<"AND "<<(a&b)<<endl;
+    cout<<"OR "<<(a|b)<<endl;
+    cout<<"NOT "<<(~a)<<endl;
+    cout<<"XOR "<<(a^b)<<endl;
+
+    return 0;
+}
